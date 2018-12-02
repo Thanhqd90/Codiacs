4d50d7e77c08372d0d089ca1aff49225dfe7b34b
Thanh Dang <Thanhqd1990@gmail.com>
2018-12-02 15:59:38 -0600

    Basic MVC folder structure started

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+var app = express();
+
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/controller");
+
+app.use(routes);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
