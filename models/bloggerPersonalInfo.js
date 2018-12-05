81ea84c2edceb1e5dddf983b361f94534752212d
Akanksha Kapoor <agaur05@gmail.com>
2018-12-04 20:05:25 -0600

    adding personal info model

--- models/bloggerPersonalInfo.js
+++ models/bloggerPersonalInfo.js
@@ -1,49 +1,88 @@
 module.exports = function(sequelize, DataTypes) {
-    var bloggerPersonalInfo = sequelize.define("bloggerPersonalInfo", {
-      firstName: {
-        type: DataTypes.String,
-        allowNull: false,
-        validate:{ 
-          is:{ 
-          args:[["^[a-z]+$",'i']],
-          msg:"Numbers not allowed."
+    let bloggerPersonalInfo = sequelize.define(
+        "bloggerPersonalInfo",
+        {
+            firstName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    is: {
+                        args: [["^[a-z]+$", "i"]],
+                        msg: "Numbers not allowed."
+                    },
+                    min: {
+                        args: [[2]],
+                        msg: "Name cannot be too short."
+                    },
+                    max: {
+                        args: [[20]],
+                        msg: "Name cannot be to long."
+                    }
+                }
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    is: {
+                        args: [["^[a-z]+$", "i"]],
+                        msg: "Numbers not allowed."
+                    },
+                    min: {
+                        args: [[2]],
+                        msg: "Name cannot be too short."
+                    },
+                    max: {
+                        args: [[20]],
+                        msg: "Name cannot be to long."
+                    }
+                }
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    isEmail: true,
+                    isLowercase: true
+                }
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    length: [5, 10]
+                }
+            },
+            password: {
+                type: Datatypes.STRING,
+                allowNull: false,
+                validate: {
+                    length: [7, 10]
+                }
+            },
+            securityQuestion: {
+                type: Datatypes.STRING,
+                allowNull: false
+            },
+            answer: {
+                type: Datatypes.STRING,
+                allowNull: false,
+                validate: {
+                    length: [1]
+                }
+            },
+            acceptTerm: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            }
         },
-        min: {
-          args:[[2]],
-          msg: "Name cannot be too short."
-       },
-       max:{
-         args:[[20]],
-         msg: "Name cannot be to long."
-       }
-      }
-    },
-    lastName: {
-      type: DataTypes.String,
-      allowNull: false,
-      validate:{ 
-        is:{ 
-        args:[["^[a-z]+$",'i']],
-        msg:"Numbers not allowed."
-      },
-      min: {
-        args:[[2]],
-        msg: "Name cannot be too short."
-     },
-     max:{
-       args:[[20]],
-       msg: "Name cannot be to long."
-     }
-    }
-  },
-  
-      exist: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      }
-  
-    });
-  
+        {
+            freezeTableName: true,
+            tableName: "bloggerPersonalInfo"
+        }
+    );
+    bloggerPersonalInfo.associate = function(models) {
+        models.bloggerPersonalInfo.hasMany(models.blogs);
+    };
     return bloggerPersonalInfo;
-  };
-  
+};
