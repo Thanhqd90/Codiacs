303a71a2b9f96073281580c8a9fa54186cd7d589
Thanh Dang <Thanhqd1990@gmail.com>
2018-12-05 18:39:37 -0600

    Started register javacsript

--- controllers/controller.js
+++ controllers/controller.js
@@ -25,8 +25,26 @@ router.get("/home", function (req, res) {
 router.get("/login", function (req, res) {
     res.render("login");
 });
+
 router.get("/register", function (req, res) {
     res.render("register");
 });
+
+router.post("/register/create", function(req, res) {
+    db.bloggerPersonalInfo.create({
+        firstName: firstName.val().trim(),
+        lastName: lastName.val().trim(),
+        username: username.val().trim(),
+        email: email.val().trim(),
+        password: password.val().trim(),
+        phone: phone.val().trim(),
+        checkbox: checkbox.val()
+    })
+      .then(function(newUser) {
+        console.log(newUser);
+        // loggedIn = true;
+        res.redirect("/home");
+      });
+  });
 
 module.exports = router;
