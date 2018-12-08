a784b2e071121f7fbb1cbef06be667b24012a390
Roger <rgbyford@gmail.com>
2018-12-08 13:49:23 -0600

    some nightmare improvements

--- test/testLogin.js
+++ test/testLogin.js
@@ -1,52 +1,98 @@
 require("mocha");
 //require("chai");
 let Nightmare = require("nightmare");
+const nightmare = null;
 
+//this.timeout("20s");
 describe("Login Page", function () {
-    this.timeout("20s");
-
-    let nightmare = null;
+    //var nightmare = null;
     beforeEach(() => {
+        console.log("beforeEach");
         // show true lets you see wth is actually happening :)
-        nightmare = new Nightmare({
+        // nightmare = new Nightmare({
+        //     show: true
+        // });
+    });
+});
+
+describe("home page click register", function () {
+    this.timeout(10000);
+    it("should work", (done) => {
+        const nightmare = new Nightmare({
             show: true
         });
+        nightmare
+            .goto("http://localhost:8080/home")
+            .click("#register")
+            .wait(3000)
+            .wait("#registerBody")
+            .end()
+            .then(function () {
+                //            .then(function (result) {
+                //                console.log("Result: ", result);
+                done();
+            })
+            .catch(done);
     });
+});
 
-    // describe("given bad data", () => {
-    //     it("should work", (done) => {
-    //         nightmare
-    //             .goto("http://localhost:8080/home")
-    //             .on("page", (type, message) => {
-    //                 console.log(message);
-    //                 if (type === "alert") {
-    //                     done();
-    //                 }
-    //             })
-    //             .click("#about")
-    //             .wait(7000)
-    //             .wait ("#about-page")
-    //             .end()
-    //             .then(function (result) {
-    //                 //console.log ("Result: ", result);
-    //                 done();
-    //             })
-    //             .catch(done);
-    //     });
-    // });
-    describe("given bad data", () => {
-        it("should work", (done) => {
-            nightmare
-                .goto("http://localhost:8080/home")
-                .click("#register")
-                .wait(7000)
-                .wait ("#registerBody")
-                .end()
-                .then(function (result) {
-                    console.log ("Result: ", result);
-                    done();
-                })
-                .catch(done);
+describe("register page incomplete", function () {
+    this.timeout(20000); // nightmare takes a long time to fill in the page
+    it("should fail", (done) => {
+        const nightmare = new Nightmare({
+            show: true
         });
+        nightmare
+            .goto("http://localhost:8080/register")
+            .type("input[id='firstName']", "Roger")
+            .type("input[id='lastName']", "Byford")
+            .type("input[id='username']", "rgbyford")
+            .type("input[id='email']", "byford@gmail.com")
+            .type("input[id='password']", "")
+            .type("input[id='answer']", "table tennis")
+            //            .click("#terms")
+            .click("#signUpButton")
+            .wait(3000)
+            .end()
+            .then(function () {
+                done();
+            })
+            .catch(done);
+    });
+});
+
+describe("register page completed", function () {
+    this.timeout(20000); // nightmare takes a long time to fill in the page
+    it("should fail", (done) => {
+        const nightmare = new Nightmare({
+            show: true
+        });
+        nightmare
+            .goto("http://localhost:8080/register")
+            .type("input[id='firstName']", "Roger")
+            .type("input[id='lastName']", "Byford")
+            .type("input[id='username']", "rgbyford")
+            .type("input[id='email']", "byford@gmail.com")
+            .type("input[id='password']", "password")
+            .type("input[id='answer']", "table tennis")
+            .click("#terms")
+            .click("#signUpButton")
+            .wait(3000)
+            //.exists("#signUpButton") // finds this
+            .exists("#home-page") // but not this
+            .end()
+            .then(function (homePage) {
+                if (homePage) {
+                    console.log("homePage"); // both of these work
+                } else {
+                    console.log("not homePage");
+                }
+                done();
+            })
+            //            .end()
+            // .then(function () {
+            //     done();
+            // })
+            .catch(done);
     });
 });
