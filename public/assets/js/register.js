ddac28a0d34dc1c18705e9878eb47504e3f73a95
Jeffreyvh <jeffreyvh@ymail.com>
2018-12-07 19:37:52 -0600

    updated commenting within files and css

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,6 +1,4 @@
-
-   
-   // $(document).ready(function() {
+// $(document).ready(function() {
 //     // jQuery references
 //     let firstName = $("#defaultRegisterFormFirstName");
 //     let lastName = $("#defaultRegisterFormLastName");
