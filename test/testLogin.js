38beb4c663570666a1a60c3112f31659d232d403
Roger <rgbyford@gmail.com>
2018-12-09 07:07:11 -0600

    nightmare beginning to work!

--- test/testLogin.js
+++ test/testLogin.js
@@ -1,7 +1,7 @@
 require("mocha");
 //require("chai");
 let Nightmare = require("nightmare");
-const nightmare = null;
+//const nightmare = null;
 
 //this.timeout("20s");
 describe("Login Page", function () {
@@ -16,7 +16,8 @@ describe("Login Page", function () {
 });
 
 describe("home page click register", function () {
-    this.timeout(10000);
+    console.log("home page");
+    this.timeout(15000);
     it("should work", (done) => {
         const nightmare = new Nightmare({
             show: true
@@ -28,41 +29,14 @@ describe("home page click register", function () {
             .wait("#registerBody")
             .end()
             .then(function () {
-                //            .then(function (result) {
-                //                console.log("Result: ", result);
                 done();
             })
             .catch(done);
     });
-});
 
-describe("register page incomplete", function () {
-    this.timeout(20000); // nightmare takes a long time to fill in the page
-    it("should fail", (done) => {
-        const nightmare = new Nightmare({
-            show: true
-        });
-        nightmare
-            .goto("http://localhost:8080/register")
-            .type("input[id='firstName']", "Roger")
-            .type("input[id='lastName']", "Byford")
-            .type("input[id='username']", "rgbyford")
-            .type("input[id='email']", "byford@gmail.com")
-            .type("input[id='password']", "")
-            .type("input[id='answer']", "table tennis")
-            //            .click("#terms")
-            .click("#signUpButton")
-            .wait(3000)
-            .end()
-            .then(function () {
-                done();
-            })
-            .catch(done);
-    });
-});
+    let dataStrings = ["Roger", "Byford", "rgbyford", "byford@gmail.com", "password", "table tennis"];
 
-describe("register page completed", function () {
-    this.timeout(20000); // nightmare takes a long time to fill in the page
+    this.timeout(18000); // nightmare takes a long time to fill in the page
     it("should fail", (done) => {
         const nightmare = new Nightmare({
             show: true
@@ -78,21 +52,44 @@ describe("register page completed", function () {
             .click("#terms")
             .click("#signUpButton")
             .wait(3000)
-            //.exists("#signUpButton") // finds this
-            .exists("#home-page") // but not this
+            .exists("#signUpButton") // finds this
+            //.exists("#home-page") // but not this
             .end()
-            .then(function (homePage) {
-                if (homePage) {
-                    console.log("homePage"); // both of these work
-                } else {
-                    console.log("not homePage");
-                }
+            .then(function () {
                 done();
             })
-            //            .end()
-            // .then(function () {
-            //     done();
-            // })
             .catch(done);
     });
+
+    console.log("incomplete");
+    let tempString = "";
+    for (let i = 0; i < dataStrings.length; i++) {
+        it("should fail", (done) => {
+            tempString = dataStrings[i];
+            dataStrings[i] = "";
+            //console.log(i, ": ", dataStrings[i]);
+            this.timeout(19000); // nightmare takes a long time to fill in the page
+            const nightmare = new Nightmare({
+                show: true
+            });
+            nightmare
+                .goto("http://localhost:8080/register")
+                .type("input[id='firstName']", dataStrings[0])
+                .type("input[id='lastName']", dataStrings[1])
+                .type("input[id='username']", dataStrings[2])
+                .type("input[id='email']", dataStrings[3])
+                .type("input[id='password']", dataStrings[4])
+                .type("input[id='answer']", dataStrings[5])
+                //            .click("#terms")
+                .click("#signUpButton")
+                .wait(3000)
+                .wait("#registerBody") // should find this
+                .end()
+                .then(function () {
+                    done();
+                })
+                .catch(done);
+            dataStrings[i] = tempString;
+        });
+    }
 });
