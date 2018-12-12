0174a40d68458f10e0db2dc803ae8d683fe60a52
Akanksha Kapoor <agaur05@gmail.com>
2018-12-11 18:10:19 -0600

    fixing pie-charts

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
         $("#sidebar").toggleClass("active");
         $(this).toggleClass("active");
     });
- });
- 
- // layout variable for email
- const space = "         |        ";
- // email function for users to contact
- // error is showing due to eslint but works fine
- function send() {
+});
+
+// layout variable for email
+const space = "         |        ";
+// email function for users to contact
+// error is showing due to eslint but works fine
+function send() {
     setTimeout(function() {
         window.open("mailto:jeffreyvh@ymail.com" + "?subject=" + document.getElementById("subject").value + "&body=" + document.getElementById("name").value + space + document.getElementById("email").value + space + document.getElementById("message").value);
     }, 320);
- }
+}
